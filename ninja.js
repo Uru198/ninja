919d977b2b7af4684f290160db66810d5d6853be
Andres Uru <98914498+Uru198@users.noreply.github.com>
2022-09-26 21:37:42 -0500

    ninja

---
+++ ninja.js
@@ -0,0 +1,29 @@
+
+class Ninja {
+    constructor(nombre, salud, velocidad, fuerza) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.velocidad = velocidad || 3;
+        this.fuerza = fuerza || 3;
+    }
+    sayName() {
+        console.log(this.nombre);
+    }
+    showStats(){
+        console.log(this.nombre,this.salud,this.velocidad,this.fuerza);
+    }
+    drinkSake(){
+        console.log(this.salud += 10);
+    }
+
+}
+
+const  ninja1 = new Ninja("Horus", 10, 7,15);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+
+
+
+
+
